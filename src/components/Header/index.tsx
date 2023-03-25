f2ee240eefc32759bbae5e33cd0fccbf660bfed0
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-25 14:26:39 -0300

    feat: add menuhamburger component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import { MenuHamburguer } from "../MenuHamburger";
 import mydrugs from "../../assets/mydrugs.png";
 import { Container } from "./style";
 
 const Header = () => {
+  const width = window.screen.width;
+
   return (
     <Container>
+      {width < 768 ? <MenuHamburguer /> : null}
       <a>SHOP</a>
       <a>FAQ</a>
       <img src={mydrugs} alt="My Drugs" />
